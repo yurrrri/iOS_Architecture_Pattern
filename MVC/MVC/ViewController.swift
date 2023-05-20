f98835190fa4ec51bfb3d8a22fefbb6bded918ae
yurrrri <37764504+yurrrri@users.noreply.github.com>
2023-05-20 21:54:36 +0900

    🎉 MVC 패턴 프로젝트 생성

---
+++ MVC/MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by 이유리 on 2023/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
